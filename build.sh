b4f747a289ea2fc9dc8f20da0134e6fff7cd7cb4
luis-lavaire <llavaire01@gmail.com>
2020-04-23 16:06:48 -0600

    Fixed build script.

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@ apt-get --yes update
 apt-get --yes install wget equivs curl git
 
 
-DEPENDS=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d; s/$/,/' src/dependencies)
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d; s/$/,/' deps)
 
-GIT_COMMIT=$(git rev-parse --short HEAD)
+git_version=$(git rev-parse --short HEAD)
 
 printf "%s\n" \
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: adduser-nitrux" \
-	"Version: 0.0.1-${GIT_COMMIT}" \
+	"Version: 0.0.1-$git_version" \
 	"Maintainer: Luis Lavaire <llavaire01@gmail.com>" \
-	"Depends: $DEPENDS" \
+	"Depends: $deps" \
 	"Architecture: all" \
 	"Description: Default user for the Nitrux system." \
 > config
