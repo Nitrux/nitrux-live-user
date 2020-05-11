ea3773a4a7a955a60f6df5ec771ee76e4e1fbbc3
luis-lavaire <llavaire01@gmail.com>
2020-05-10 18:21:38 -0600

    make this simpler.

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
-apt -q -yy update
-apt -q -yy install wget equivs curl git
-
-
-deps=$(sed -e '/^#.*$/d; /^\s*$/d; $! s/$/,/' deps)
-
 git_version=$(git rev-parse --short HEAD)
 
 > config printf "%s\n" \
@@ -13,7 +7,7 @@ git_version=$(git rev-parse --short HEAD)
 	"Package: nitrux-live-user" \
 	"Version: 0.0.1-$git_version" \
 	"Maintainer: Luis Lavaire <llavaire01@gmail.com>" \
-	"Depends: $deps" \
+	"Depends: nitrux-minimal,adduser" \
 	"Architecture: all" \
 	"Description: Default user for the Nitrux system."
 
