2d667ebb3c7241c56150a8b7c20f053a832c9740
Uri Herrera <hello@uriherrera.com>
2021-04-22 09:27:30 -0500

    sigh...

--- build.sh
+++ build.sh
@@ -10,11 +10,12 @@ git_version=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-live-user" \
-	"Version: 0.1.2-$git_version" \
+	"Version: 0.1.3-$git_version" \
 	"Maintainer: Luis Lavaire <llavaire01@gmail.com>" \
 	"Depends: adduser" \
 	"Architecture: all" \
 	"Postinst: postinst" \
+	"Postrm: postrm" \
 	"Description: Default user for the Nitrux system."
 
 equivs-build config
