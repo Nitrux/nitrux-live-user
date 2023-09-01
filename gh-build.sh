a3f217de12d916de3e9e329b0d8686261a58b694
UriHerrera <hello@uriherrera.com>
2023-09-01 04:14:18 -0600

    do not add live user to sudo group

--- gh-build.sh
+++ gh-build.sh
@@ -10,7 +10,7 @@ git_version=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-live-user" \
-	"Version: 0.2.4-$git_version" \
+	"Version: 0.2.6-$git_version" \
 	"Maintainer: Luis Lavaire <llavaire01@gmail.com>" \
 	"Depends: adduser" \
 	"Architecture: all" \
