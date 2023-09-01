d2746c3eecf920ef8fd406fa861815db8bada989
UriHerrera <hello@uriherrera.com>
2023-09-01 02:41:19 -0600

    update pkg

--- gh-build.sh
+++ gh-build.sh
@@ -10,12 +10,12 @@ git_version=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-live-user" \
-	"Version: 0.2.3-$git_version" \
+	"Version: 0.2.4-$git_version" \
 	"Maintainer: Luis Lavaire <llavaire01@gmail.com>" \
 	"Depends: adduser" \
 	"Architecture: all" \
 	"Postinst: postinst" \
 	"Postrm: postrm" \
-	"Description: Default user for the Nitrux system."
+	"Description: Default Live user for Nitrux OS."
 
 equivs-build config
